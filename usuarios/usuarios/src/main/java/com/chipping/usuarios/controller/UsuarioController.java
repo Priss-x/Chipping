5a61cb7408f7a9b4763338638c1dfba389c3bcf1
Priss <priscilacontreras54@gmail.com>
2026-05-26 21:24:18 -0400

    microservicio de usuarios/login completo
    Usuarios/login check completo.
    Corriendo puerto 8093, validaciones con Postman ok.

---
+++ usuarios/usuarios/src/main/java/com/chipping/usuarios/controller/UsuarioController.java
@@ -0,0 +1,44 @@
+package com.chipping.usuarios.controller;
+
+import com.chipping.usuarios.dto.LoginRequest;
+import com.chipping.usuarios.model.Usuario;
+import com.chipping.usuarios.service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<?> registrar(@Valid @RequestBody Usuario usuario) {
+        try {
+            Usuario nuevoUsuario = usuarioService.registrarUsuario(usuario);
+            return ResponseEntity.ok(nuevoUsuario);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+@PostMapping("/login")
+    public ResponseEntity<?> login (@RequestBody LoginRequest loginRequest) {
+        try {
+            Usuario usuarioValido = usuarioService.verificarLogin(
+                    loginRequest.getUsername(),
+                    loginRequest.getPassword()
+            );
+            return ResponseEntity.ok(usuarioValido);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
---
+++ usuarios/usuarios/usuarios/src/main/java/com/chipping/usuarios/service/UsuarioService.java
@@ -0,0 +1,35 @@
+package com.chipping.usuarios.service;
+
+import com.chipping.usuarios.model.Usuario;
+import com.chipping.usuarios.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuario registrarUsuario(Usuario usuario) {
+        if (usuarioRepository.findByEmail(usuario.getEmail()).isPresent()) {
+            throw new RuntimeException("El correo ya está registrado");
+        }
+        if (usuarioRepository.findByUsername(usuario.getUsername()).isPresent()) {
+            throw new RuntimeException("El nombre de usuario ya existe");
+        }
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario verificarLogin (String username, String password) {
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("El usuario no existe"));
+        if (!usuario.getPassword().equals(password)) {
+            throw new RuntimeException("Contraseña incorrecta");
+        }
+        return usuario;
+    }
+
+}
